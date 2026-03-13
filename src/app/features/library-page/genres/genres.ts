9a78f60c58393fe3658ec39386a7203f14cea6aa
evgeniia-korolova <jkod@ukr.net>
2026-03-13 17:42:39 +0200

    feat: implement burger for small and medium screens for genres page

--- src/app/features/library-page/genres/genres.ts
+++ src/app/features/library-page/genres/genres.ts
@@ -5,13 +5,13 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddGenreDialog } from '../../add-genre-dialog/add-genre-dialog';
 import { Genre } from '../../../core/models/genre.model';
 import { MatButtonModule } from '@angular/material/button';
-
 import { MatIconModule } from '@angular/material/icon';
-import { SortOrder } from '../../../core/models/sort-order.type';
 import { GenresBooksList } from './genres-books-list/genres-books-list';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { SidebarService } from '../../../core/services/sidebar-service';
+import { ResponsiveService } from '../../../core/services/responsive-service';
 
 @Component({
   selector: 'app-genres',
@@ -23,9 +23,10 @@ import { debounceTime, distinctUntilChanged } from 'rxjs';
 export class Genres {
   protected readonly libraryStore = inject(LibraryStore);
   private dialog = inject(MatDialog);
-  sortOrder = signal<SortOrder>('none');
-  searchControl = new FormControl('');
-  searchTerm = signal('');
+  protected readonly sidebarService = inject(SidebarService);
+  protected readonly responsiveService = inject(ResponsiveService);
+  protected searchControl = new FormControl('');
+  protected readonly searchTerm = signal('');
 
   openAddGenreDialog() {
     const dialogRef = this.dialog.open(AddGenreDialog, {
@@ -42,6 +43,7 @@ export class Genres {
 
   onSelectGenre(title: string) {
     this.libraryStore.selectGenre(title);
+    this.sidebarService.closeSidebar();
   }
 
   searchTermFromInput = toSignal(
--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,7 +1,19 @@
-import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
+import { SidebarService } from '../../../core/services/sidebar-service';
+import { ResponsiveService } from '../../../core/services/responsive-service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +22,22 @@ import { MatToolbar } from '@angular/material/toolbar';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  protected readonly sidebarService = inject(SidebarService);
+  protected readonly responsiveService = inject(ResponsiveService);
+  private router = inject(Router);
+
   protected readonly title = signal('authors-books-app');
-  currentTheme = input.required<string>();
+  protected readonly currentTheme = input.required<string>();
   toggleTheme = output<void>();
-  toggleSidebar = output<void>();
+  private currentUrl = signal(this.router.url);
+
+  constructor() {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e) => {
+        this.currentUrl.set(e.urlAfterRedirects);
+      });
+  }
+
+  protected readonly isGenresPage = computed(() => this.currentUrl()?.includes('/genres'));
 }
